657eff361e4f58b8d014be0f1f69fef2822017b6
Dziyana Kantsevich <53045550+dinak0103@users.noreply.github.com>
2025-04-08 23:18:52 +0300

    add overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('toggle'),
-      navClose = document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close'),
+      overlay = document.getElementById('overlay');
 
 if(navToggle) {
     navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('show-menu');
+        overlay.style.display = 'block';
     })
 }
 
 if(navClose){
     navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
+        navMenu.classList.remove('show-menu');
+        overlay.style.display = 'none';
     })
 }
-
-
 
 const navLink = document.querySelectorAll('.menu__link')
 
