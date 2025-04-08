9b975f8531fd64f0a29a29ce0103f7b843a06459
Dziyana Kantsevich <53045550+dinak0103@users.noreply.github.com>
2025-04-08 23:05:15 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+
+const navLink = document.querySelectorAll('.menu__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
